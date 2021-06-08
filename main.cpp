4c07de135ad1b6cd68ef804d29f931e95d79432d
CI31X <dkonstantinov0@gmail.com>
2021-06-08 08:02:31 +0300

    init file

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> a(4);
+    double x, x_dot, teta, teta_dot;
+    double delta_t = 0.1;
+    double x_end; // finish
+    //std::cout << "MAN HELLOW THIS IS 1 COMMIT" << std::endl;
+    return 0;
+}
