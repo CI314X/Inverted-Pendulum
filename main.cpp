dc60a05ac19c255618bc43ed003ce3ef337f4bb7
Daniil <user@DESKTOP-BLKAU73.localdomain>
2021-06-10 14:18:19 +0300

    starting ODE

--- main.cpp
+++ main.cpp
@@ -2,28 +2,23 @@
 #include <iostream>
 #include <ct/optcon/optcon.h>  // also includes ct_core
 #include <Eigen/Dense>
-
+#include <cmath>
 int main(int argc, char** argv)
 {
     const size_t state_dim = 4;
     const size_t control_dim = 1;
-    ct::core::StateVector<state_dim> x;  // state
-
-    x.setZero();
-    x(3) = -0.01; // angle deviation
+    
 
     ct::core::ControlVector<control_dim> u;  // control
     u.setZero();
     
-    // parameters 
+    // parameters of an environment and Cart
+    // system CI
     double m = 1.0;
     double M = 5.0; // mass of a Cart
     double L = 2.0; // length
     double g = 9.81;
     double d = 0.01; // coefficient of a friction
-    double dt = 0.001;
-    double t = 0.0;
-    size_t nSteps = 100;
 
     Eigen::Matrix<double, state_dim, state_dim> A;
     A.row(0) << 0.0, 1.0, 0.0, 0.0;
@@ -55,8 +50,56 @@ int main(int argc, char** argv)
     lqrSolver.compute(Q, R, A, B, K, RisDiagonal, solveRiccatiIteratively);
     std::cout << "LQR gain matrix:" << std::endl << K << std::endl;
 
-    //integrator.integrate_n_steps(x, t0, nSteps, dt);
-    // print the new state
-    //std::cout << "state after integration: " << x.transpose() << std::endl;
+    double dt = 0.1; // c
+    double T = 3;
+    size_t nSteps = 1000;
+    ct::core::StateVector<state_dim> x0;  // initial state
+    x0.setZero();
+    x0(0) = -1.0;
+    x0(3) = -0.01; // angle deviation
+
+    ct::core::StateVector<state_dim> x1;  // finish state
+    x1.setZero();
+    x1(0) = 1.0;
+    x1(3) = 0.0;
+    // u = -K*(x_finish - x0)   !! возможно, не нужно делать B*K
+    // dx/dt = A*x + B* u
+    // проверка - посмотреть на графики x(3)=theta (не должен отклоняться от нуль сильно) и x(1) = x (должен пройти от -1 до 1)
+
+    // initial conditions
+    const size_t len_x = 30; // =round(T / dt)
+
+    Eigen::Matrix<double, len_x, state_dim> x;
+    x.setZero();
+   // std::cout << x << std::endl;
+    //std::cout << length(x) << std::endl;
+    for (size_t i = 0; i < len_x; ++i)
+    {
+        //std::cout << i << x.row(i) << std::endl;
+        if (i < round(len_x / 2))
+        {
+            x.row(i) << x0(0), x0(1), x0(2), x0(3);
+        }
+        else
+        {
+            x.row(i) << x1(0), x1(1), x1(2), x1(3);
+        }
+    }
+    std::cout << x << std::endl;
+    double t = 0.0;
+    while (t <= T)
+    {
+        // по всему пространству
+        for (size_t i = 0; i < len_x; ++i)
+        {
+            x(4 * (i + 1)) = x(4 * i) + dt * x(4 * i + 1);
+            x(4 * (i + 1) + 1) = x(4 * i + 1) + dt * ( A(6) * x(4 * i + 3))
+        }
+
+
+        t += dt;
+    }
+
+
     return 0;
 }
