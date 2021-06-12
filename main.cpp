4b083b33669af69d248e98d47811e87ce7acb75a
Daniil <user@DESKTOP-BLKAU73.localdomain>
2021-06-13 01:05:08 +0300

    fix comments

--- main.cpp
+++ main.cpp
@@ -16,11 +16,11 @@ int main(int argc, char** argv)
     
     // parameters of an environment and Cart
 
-    double m = 1.0;
-    double M = 5.0; // mass of the Cart
-    double L = 2.0; // length
-    double g = 9.81;
-    double d = 1.0; // coefficient of friction
+    double m = 1.0;                          //  video - 1.0
+    double M = 5.0; // mass of the Cart          video - 5.0
+    double L = 2.0; // length                    video - 2.0
+    double g = 9.81;                         //  video - 10.0
+    double d = 1.0; // coefficient of friction   video - 1.0
 
     Eigen::Matrix<double, state_dim, state_dim> A;
     A.row(0) << 0.0, 1.0, 0.0, 0.0;
@@ -55,20 +55,12 @@ int main(int argc, char** argv)
     //std::cout << "LQR gain matrix:" << std::endl << K << std::endl;
     //std::cout << (A - B * K).eigenvalues() << std::endl << std::endl;
 
-    ct::core::StateVector<state_dim> x0;  // initial state
+    ct::core::StateVector<state_dim> x0;  // initial state - x, x_dot, theta, theta_dot
     x0 << atof(argv[1]), atof(argv[2]), atof(argv[3]), atof(argv[4]);
-
-    //x0(0) = 0.0;
-    //x0(1) = 0.0;
-    //x0(2) = 0.0; // angle deviation
-    //x0(3) = 0.5;
     std::cout << "Initial state     : " << x0(0) << "\t" << x0(1) << "\t" << x0(2) << "\t" << x0(3) << std::endl;
-    ct::core::StateVector<state_dim> x1;  // final state
-    //x1(0) = 1.0;
-    x1(0) = atof(argv[5]);
-    x1(1) = 0.0;
-    x1(2) = 0.0;
-    x1(3) = 0.0;
+
+    ct::core::StateVector<state_dim> x1;  // final state - x, x_dot, theta, theta_dot
+    x1 << atof(argv[5]), 0.0, 0.0, 0.0;
     std::cout << "Wanted Final state: " << x1(0) << "\t" << x1(1) << "\t" << x1(2) << "\t" << x1(3) << std::endl;
 
     double t = 0.0;
@@ -79,15 +71,14 @@ int main(int argc, char** argv)
 
     std::ofstream fout; // file
     fout.open("out1.txt"); 
-    fout << "t\tx\tx_dot\ttheta\ttheta_dot\n";
+    fout << "t\tx\tx_dot\ttheta\ttheta_dot\tcontrol\n";
 
     double eps = 1e-4;  // accuracy of the final position for L2 norm
     y = x0;
     while ((y - x1).squaredNorm() >= eps)
     {
-        fout << t << "\t" << std::setprecision(4) << y(0) << "\t" << y(1) << "\t" << y(2) << "\t" << y(3) << "\n";
-        
         yn = y + dt * (A * y - B * K * (y - x1));
+        fout << t << "\t" << std::setprecision(4) << y(0) << "\t" << y(1) << "\t" << y(2) << "\t" << y(3) << "\t" << -K * (y - x1) <<"\n";
         y = yn;
         t += dt;
     }
